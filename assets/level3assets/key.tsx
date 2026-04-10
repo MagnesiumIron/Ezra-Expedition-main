e01003e21208754436ca464fdb44beb5f400cbeb
MagnesiumIron <margaretashleyvalenzuela.9@gmail.com>
2026-04-10 16:40:50 +0800

    added lvl3 for reference

---
+++ assets/level3assets/key.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="key" tilewidth="32" tileheight="32" tilecount="12" columns="12">
+ <image source="tileset_forest/TILED_files/key.png" width="384" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="3" duration="120"/>
+   <frame tileid="4" duration="120"/>
+   <frame tileid="5" duration="120"/>
+   <frame tileid="6" duration="120"/>
+   <frame tileid="7" duration="120"/>
+   <frame tileid="8" duration="120"/>
+   <frame tileid="9" duration="120"/>
+   <frame tileid="10" duration="120"/>
+   <frame tileid="11" duration="120"/>
+   <frame tileid="11" duration="120"/>
+   <frame tileid="10" duration="120"/>
+   <frame tileid="9" duration="120"/>
+   <frame tileid="8" duration="120"/>
+   <frame tileid="7" duration="120"/>
+   <frame tileid="6" duration="120"/>
+   <frame tileid="5" duration="120"/>
+   <frame tileid="4" duration="120"/>
+   <frame tileid="3" duration="120"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="0" duration="120"/>
+  </animation>
+ </tile>
+</tileset>
